5062361e07bae21fd874ce49fc24b7ea3258e633
minjoo <kim9min3joo@gmail.com>
2020-12-18 20:48:41 +0900

    homework_final
    최종본 제출

--- homework_week3_salty.py
+++ homework_week3_salty.py
@@ -33,30 +33,4 @@ for tr in trs:
         }
 
         db.musics.insert_one(data)
-# soup = BeautifulSoup(data.text, 'html.parser')
-#
-# trs = soup.select('#body-content > div.newest-list > div> table > tbody > tr')
-#
-# for tr in trs:
-#     rank = tr.select_one('td.number')
-#     rank_arrange = rank.text.strip()[0:2].rstrip()
-#
-#     title = tr.select_one('td.info > a.title')
-#     title_arrange = title.text.strip()
-#
-#     artist = tr.select_one('td.info > a.artist')
-#     artist_arrange = artist.text
-#
-#     # print(rank_arrange, title_arrange, artist_arrange)
-#     if rank_arrange is not None and title_arrange is not None and artist_arrange is not None:
-#         data = {
-#             'rank': rank_arrange,
-#             'title': title_arrange,
-#             'artist': artist_arrange
-#         }
-#
-#         db.musics.insert_one(data)
 
-
-musics_all = list(db.musics.find({}, {'_id':False}))[0]
-print(musics_all)
